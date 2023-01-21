569ffd0f6eebb11e10d3789c5e9bbdd391bfb499
carraes <carraeshb@gmail.com>
2023-01-21 13:49:10 -0300

    feat: basic css and routes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import Game from './pages/Game'
+import Login from './pages/Login'
+import Settings from './pages/Settings'
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/settings" element={<Settings />} />
+    </Routes>
   )
 }
 
