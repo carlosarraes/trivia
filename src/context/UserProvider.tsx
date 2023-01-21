e4ad1be1fd771ee9c4c22d18bc2a673320290177
carraes <carraeshb@gmail.com>
2023-01-21 19:04:24 -0300

    feat: create UserProvider

---
+++ src/context/UserProvider.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import UserContext from './UserContext'
+
+export type UserContextType = {
+  user: {
+    token: string
+    name: string
+    email: string
+    score: number
+  }
+  setUser: React.Dispatch<React.SetStateAction<{}>>
+}
+
+function UserProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState({
+    token: '',
+    name: '',
+    email: '',
+    score: 0,
+  })
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+}
+
+export default UserProvider
