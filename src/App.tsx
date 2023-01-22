f2bb60a672c616afdf8ff7842c41834f2b3d8a99
carraes <carraeshb@gmail.com>
2023-01-22 11:35:04 -0300

    feat: route to feedback page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import Feedback from './pages/Feedback'
 import Game from './pages/Game'
 import Login from './pages/Login'
 import Settings from './pages/Settings'
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/game" element={<Game />} />
       <Route path="/settings" element={<Settings />} />
+      <Route path="/feedback" element={<Feedback />} />
     </Routes>
   )
 }
