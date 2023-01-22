494e29f50da0ee2011a4df0368542df9645d8c4a
carraes <carraeshb@gmail.com>
2023-01-22 11:33:26 -0300

    refactor: optional params

--- src/utils/fetchServices.ts
+++ src/utils/fetchServices.ts
@@ -1,3 +1,5 @@
+import { Md5 } from 'ts-md5'
+
 export const fetchToken = async () => {
   const response = await fetch('https://opentdb.com/api_token.php?command=request')
   const data = await response.json()
@@ -6,9 +8,9 @@ export const fetchToken = async () => {
 
 export const fetchQuestions = async (
   token: string,
-  category: string,
-  difficulty: string,
-  type: string,
+  category?: string,
+  difficulty?: string,
+  type?: string,
 ) => {
   let url = 'https://opentdb.com/api.php?amount=5'
   if (category) url += `&category=${category}`
@@ -18,4 +20,10 @@ export const fetchQuestions = async (
   const response = await fetch(`${url}&token=${token}`)
   const data = await response.json()
   return data
+}
+
+export const fetchGravatar = async (email: string) => {
+  const hash = Md5.hashStr(email)
+  const response = await fetch(`https://www.gravatar.com/avatar/${hash}`)
+  return response.url
 }
