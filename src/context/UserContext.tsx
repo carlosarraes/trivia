50f6af3392945fecc9ed987c08a57979c3f2acfa
carraes <carraeshb@gmail.com>
2023-01-24 14:59:40 -0300

    feat: settings Context/Provider

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,12 +1,21 @@
 import { createContext } from 'react'
+import { UserContextType } from './UserProvider'
 
-const UserContext = createContext({
+const UserContext = createContext<UserContextType>({
   user: {
+    token: '',
     name: '',
+    email: '',
     gravatar: '',
     score: 0,
-    token: '',
   },
+  settings: {
+    category: '',
+    difficulty: '',
+    type: '',
+  },
+  setUser: () => {},
+  setSettings: () => {},
 })
 
 export default UserContext
--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -5,23 +5,38 @@ export type UserContextType = {
   user: {
     token: string
     name: string
-    email: string
+    email?: string
     gravatar: string
     score: number
   }
-  setUser: React.Dispatch<React.SetStateAction<{}>>
+  settings: {
+    category: string
+    difficulty: string
+    type: string
+  }
+  setUser: React.Dispatch<React.SetStateAction<UserContextType['user']>>
+  setSettings: React.Dispatch<React.SetStateAction<UserContextType['settings']>>
 }
 
 function UserProvider({ children }: { children: React.ReactNode }) {
-  const [user, setUser] = useState({
+  const [user, setUser] = useState<UserContextType['user']>({
     token: '',
     name: '',
     email: '',
     gravatar: '',
     score: 0,
   })
+  const [settings, setSettings] = useState<UserContextType['settings']>({
+    category: '',
+    difficulty: '',
+    type: '',
+  })
 
-  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>
+  return (
+    <UserContext.Provider value={{ user, setUser, settings, setSettings }}>
+      {children}
+    </UserContext.Provider>
+  )
 }
 
 export default UserProvider
